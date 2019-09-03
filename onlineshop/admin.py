ad93b279136c28c04d588906f6cf507ea29545bf
Eric Odhiambo <odhiamboeric0049@gmail.com>
2019-09-03 16:09:58 +0300

    admin model updates

--- onlineshop/admin.py
+++ onlineshop/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from .models import CarBrand, CarModel, CarModelInstance
+
 # Register your models here.
+# To change how a model is displayed in the admin interface you define a ModelAdmin class (which describes the layout) and register it with the model.
+
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+
+
+@admin.register(CarBrand)
+class CarBrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'logo')
+    inlines = [CarModelInline]
+
+class CarModelInstanceInline(admin.TabularInline):
+    model = CarModelInstance
+
+
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'price')
+    inlines = [CarModelInstanceInline]
+
+# def display_quantity(self, obj):
+# """Return the quantity of this car's instances"""
+#         return obj.__class__.objects.count()
+#     display_quantity.short_description = 'Quantity'
+
+
+@admin.register(CarModelInstance)
+class CarModelInstanceAdmin(admin.ModelAdmin):
+    fields = ['car']
--- onlineshop/models.py
+++ onlineshop/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
 # Create your models here.
-class Brand(models.Model):
+class CarBrand(models.Model):
     """Model representing a car's brand/manufacturer"""       
     name = models.CharField(max_length=200, help_text='Enter a car brand (e.g. Toyota)')
     logo = models.ImageField(upload_to='media/images/logos', help_text='Upload brand logo')
@@ -12,20 +12,26 @@ class Brand(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+    
+    # def display_quantity(self):
+    #     """Should return the quantity of cars of this brand(e.g all Toyota cars)"""
+    #     return self.__class__.objects.count()
+
+    # display_quantity.short_description = 'Quantity'
 
 
-class Car(models.Model):
+class CarModel(models.Model):
     """Model representing a car (but not a specific copy of a car)."""
     name = models.CharField(max_length=200)
 
     # Foreign Key used because car can only have one brand, but brands can have multiple cars
     # car as a string rather than object because it hasn't been declared yet in the file
-    brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True, help_text='Enter the specific model of the car')
+    brand = models.ForeignKey('CarBrand', on_delete=models.SET_NULL, null=True, help_text='Enter the specific model of the car')
     price = models.IntegerField(help_text='Enter price of the car')
     
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the car')
     
-    photo = models.ImageField(upload_to='media/photos/cars', help_text='Upload the image of the car')
+    photo = models.ImageField(upload_to='media/images/cars', help_text='Upload the image of the car')
     
     def __str__(self):
         """String for representing the Model object."""
@@ -34,12 +40,18 @@ class Car(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this car."""
         return reverse('car-detail', args=[str(self.id)])
+    
+    # def display_quantity(self):
+    #     """Return the quantity of a specific car's instances e.g All Toyota Corolla cars"""
+    #     return self.__class__.objects.count()
+
+    # display_quantity.short_description = 'Quantity'
 
 
-class BookInstance(models.Model):
+class CarModelInstance(models.Model):
     """Model representing a specific copy of a car (i.e. that can be bought from the shop)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular car across whole shop')
-    car = models.ForeignKey('Car', on_delete=models.SET_NULL, null=True)
+    car = models.ForeignKey('CarModel', on_delete=models.SET_NULL, null=True)
 
     PURCHASE_STATUS = (
         ('a', 'Available'),
